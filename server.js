d4085a53e719df479a26fb83737bd3defba8382e
Sabbir1476 <sabbirr.ahmed512@gmail.com>
2026-05-06 00:45:24 +0600

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(helmet());
 // ── CORS ────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://portfolio-frontend-mu-taupe.vercel.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
